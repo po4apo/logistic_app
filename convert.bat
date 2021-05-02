7db30f460d34e0d3cd20ea5bbd0810aa96ec81a7
balabin <190064@edu.psuti.ru>
2021-05-02 23:22:21 +0400

    "home page, 0.1"

---
+++ convert.bat
@@ -0,0 +1,4 @@
+"C:\Users\commo\.conda\envs\pyqt5_env\Scripts\pyuic5.exe" -x ui_main.ui -o ui_main.py
+"C:\Users\commo\.conda\envs\pyqt5_env\Scripts\pyuic5.exe" -x ui_dialog.ui -o ui_dialog.py
+"C:\Users\commo\.conda\envs\pyqt5_env\Scripts\pyuic5.exe" -x ui_error.ui -o ui_error.py
+pause
